9aa69cc29d32622621cd24954ccf84cc6ef722a9
Snowman <icemaninho@yandex.ru>
2022-10-26 10:26:20 +0300

    changes java to 11

--- auth_service/src/main/java/com/snowman/auth_service/AuthServiceApplication.java
+++ auth_service/src/main/java/com/snowman/auth_service/AuthServiceApplication.java
@@ -14,5 +14,6 @@ public class AuthServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
+        System.out.println("This for check");
     }
 }
