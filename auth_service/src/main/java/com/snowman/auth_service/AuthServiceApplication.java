065e20aaca3e5ceae0bb0d8108be8eeaf3c3fe18
Snowman <icemaninho@yandex.ru>
2022-10-26 10:42:36 +0300

    delet unecessary sout

--- auth_service/src/main/java/com/snowman/auth_service/AuthServiceApplication.java
+++ auth_service/src/main/java/com/snowman/auth_service/AuthServiceApplication.java
@@ -14,6 +14,5 @@ public class AuthServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
-        System.out.println("This for check");
     }
 }
